5c1e6c9dd03215524f62368ee6217313b2075724
João Carvalho <jtacopt@gmail.com>
2023-02-10 23:37:47 +0000

    Fix nuget Adapter

--- CSeleniumFramework/Runner.cs
+++ CSeleniumFramework/Runner.cs
@@ -1,31 +1,16 @@
-﻿using CSeleniumFramework.GUI.POM;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSeleniumFramework
 {
-     class Runner
+    public class Runner
     {
 
-        public void Test()
+        public static void main()
         {
             IWebDriver driver = new ChromeDriver();
-            try
-            {
-                driver.Navigate().GoToUrl(Config.BaseUrl);
-                CookiePom cookie = new CookiePom(driver);
-                cookie.AcceptAll();
-                
-            }
-            finally
-            {
-                driver.Quit();
-            }
+            Thread.Sleep(5000);
+            driver.Quit();
         }
     }
 }
---
+++ CSeleniumFramework/test-cases/TestCase1.cs
@@ -0,0 +1,30 @@
+﻿using CSeleniumFramework.GUI.POM;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace CSeleniumFramework.test_cases
+{
+    internal class TestCase1
+    {
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Cenas");
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                driver.Navigate().GoToUrl(Config.BaseUrl);
+                CookiePom cookie = new CookiePom(driver);
+                cookie.AcceptAll();
+
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+
+
+}
